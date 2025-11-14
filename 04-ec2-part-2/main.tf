d75b8bada6e977e1d43ac706f55895d320cfde6d
Manoj-K <k_manoj@apple.com>
2025-11-14 16:59:28 +0530

    updated the tags

--- 04-ec2-part-2/main.tf
+++ 04-ec2-part-2/main.tf
@@ -32,10 +32,16 @@ resource "aws_security_group" "webtrafic" {
         protocol = "-1"
         cidr_blocks = ["0.0.0.0/0"]
     }
+    tags = {
+        Name = "Terraform-security-group"
+    }
 }
 
 resource "aws_instance" "myinstance" {
     ami = "ami-04f9aa2b7c7091927"
     instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.webtrafic.id]
+    tags = {
+        Name = "Terraform-ec2"
+    }
 }
